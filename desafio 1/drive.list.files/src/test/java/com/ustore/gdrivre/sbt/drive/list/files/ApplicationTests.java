3338940952addf2a3146d3a9b3a29cfe2b95edf2
rafaelguimaraes3 <rafael.guimaraes.tavares@gmail.com>
2020-06-22 00:39:35 -0300

    inicial

---
+++ desafio 1/drive.list.files/src/test/java/com/ustore/gdrivre/sbt/drive/list/files/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ustore.gdrivre.sbt.drive.list.files;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
